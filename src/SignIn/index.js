781ef0c0facc9ec197a17ea2bcbac8a9befeacab
sansqra <sqrasan@gmail.com>
2020-12-22 21:43:33 +0530

    final 22/12/20

--- src/SignIn/index.js
+++ src/SignIn/index.js
@@ -103,10 +103,7 @@ export default function SignIn (props) {
             <Grid item xs={false} sm={4} md={7} className={classes.image} />
             <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
               <div className={classes.paper}>
-                {/* <Typography component="h1" variant="h7">
-                  GS-Suite
-                </Typography> */}
-                <img src={"./logo.jpg"} width="300" height="120"></img>
+                <img src={"./logo.jpg"} width="300" height="80"></img>
               </div>
           
               <div className={classes.paper}>
